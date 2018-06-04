c9ac008f96df43434e25deae22e2f5b555457847
kimchoyoung <kimchoyoung7@naver.com>
2018-06-04 17:39:11 +0900

    Kimchoyoung-Strategy Patterns
    the classes in this package were not applied to strategy pattern
    this classes checks about annotation. if there are no annotations, it will return error msg.
    
    but the error msg and the ways of checking errors can be changed later.
    it violates OCP.
    
    So, I made interface to check the kinds of annotation error and divided class then
    made them inherit relations. this pattern is called strategy patterns.

--- jOOQ-checker/src/main/java/org/jooq/checker/SQLDialectChecker.java
+++ jOOQ-checker/src/main/java/org/jooq/checker/SQLDialectChecker.java
@@ -37,19 +37,7 @@
  */
 package org.jooq.checker;
 
-import static com.sun.source.util.TreePath.getPath;
-import static java.util.Arrays.asList;
-import static org.checkerframework.javacutil.TreeUtils.elementFromDeclaration;
-import static org.checkerframework.javacutil.TreeUtils.elementFromUse;
-import static org.checkerframework.javacutil.TreeUtils.enclosingMethod;
-
 import java.io.PrintWriter;
-import java.util.EnumSet;
-
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ExecutableElement;
-
-import org.jooq.Allow;
 import org.jooq.Require;
 import org.jooq.SQLDialect;
 import org.jooq.Support;
@@ -66,92 +54,28 @@ import com.sun.source.tree.MethodInvocationTree;
  */
 public class SQLDialectChecker extends AbstractChecker {
 
-    @Override
-    protected SourceVisitor<Void, Void> createSourceVisitor() {
-        return new SourceVisitor<Void, Void>(getChecker()) {
+	@Override
+	protected SourceVisitor<Void, Void> createSourceVisitor() {
+		return new SourceVisitor<Void, Void>(getChecker()) {
 
-            @Override
-            public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
-                try {
-                    ExecutableElement elementFromUse = elementFromUse(node);
-                    Support support = elementFromUse.getAnnotation(Support.class);
+			@Override
+			public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
+				try {
 
-                    // In the absence of a @Support annotation, or if no SQLDialect is supplied,
-                    // all jOOQ API method calls will type check.
-                    if (support != null && support.value().length > 0) {
-                        Element enclosing = elementFromDeclaration(enclosingMethod(getPath(root, node)));
-
-                        EnumSet<SQLDialect> supported = EnumSet.copyOf(asList(support.value()));
-                        EnumSet<SQLDialect> allowed = EnumSet.noneOf(SQLDialect.class);
-                        EnumSet<SQLDialect> required = EnumSet.noneOf(SQLDialect.class);
-
-                        boolean evaluateRequire = true;
-                        while (enclosing != null) {
-                            Allow allow = enclosing.getAnnotation(Allow.class);
-
-                            if (allow != null)
-                                allowed.addAll(asList(allow.value()));
-
-                            if (evaluateRequire) {
-                                Require require = enclosing.getAnnotation(Require.class);
-
-                                if (require != null) {
-                                    evaluateRequire = false;
-
-                                    required.clear();
-                                    required.addAll(asList(require.value()));
-                                }
-                            }
-
-                            enclosing = enclosing.getEnclosingElement();
-                        }
-
-                        if (allowed.isEmpty())
-                            error(node, "No jOOQ API usage is allowed at current scope. Use @Allow.");
-
-                        if (required.isEmpty())
-                            error(node, "No jOOQ API usage is allowed at current scope due to conflicting @Require specification.");
-
-                        boolean allowedFail = true;
-                        allowedLoop:
-                        for (SQLDialect a : allowed) {
-                            for (SQLDialect s : supported) {
-                                if (a.supports(s)) {
-                                    allowedFail = false;
-                                    break allowedLoop;
-                                }
-                            }
-                        }
-
-                        if (allowedFail)
-                            error(node, "The allowed dialects in scope " + allowed + " do not include any of the supported dialects: " + supported);
-
-                        boolean requiredFail = false;
-                        requiredLoop:
-                        for (SQLDialect r : required) {
-                            for (SQLDialect s : supported)
-                                if (r.supports(s))
-                                    continue requiredLoop;
-
-                            requiredFail = true;
-                            break requiredLoop;
-                        }
-
-                        if (requiredFail)
-                            error(node, "Not all of the required dialects " + required + " from the current scope are supported " + supported);
-                    }
-                }
-                catch (final Exception e) {
-                    print(new Printer() {
-                        @Override
-                        public void print(PrintWriter t) {
-                            e.printStackTrace(t);
-                        }
-                    });
-                }
-
-                return super.visitMethodInvocation(node, p);
-            }
-        };
-    }
+					String error_msg=methodinvocation.checker(node,p,root);
+					if(error_msg!=null);
+					error(node, error_msg);
+				}
+				catch (final Exception e) {
+					print(new Printer() {
+						@Override
+						public void print(PrintWriter t) {
+							e.printStackTrace(t);
+						}
+					});
+				}
+				return super.visitMethodInvocation(node, p);
+			}
+		};
+	}
 }
